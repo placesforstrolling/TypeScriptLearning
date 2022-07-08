364d216f96f1059fe563b771d196b0a4215f2e6b
Mustafa <mustafaanvarov1@gmail.com>
2022-07-08 18:29:34 +0500

    Lesson 34

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,17 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
+let account = new Account(1, 'Mo', 0);
+console.log(account.getBalance());
 //# sourceMappingURL=index.js.map
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+class Account {
+    readonly id: number;
+    owner: string;
+    private _balance: number;
+    nickname?: string;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        
+        this._balance += amount; 
+    }
+
+    getBalance(): number {
+        return  this._balance;
+    }
+}
+
+let account = new Account(1, 'Mo', 0);
+console.log(account.getBalance());
