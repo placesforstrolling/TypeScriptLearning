11b9268498139b59a962e0453eeafb22ef2db790
Mustafa <mustafaanvarov1@gmail.com>
2022-07-13 11:11:30 +0500

    Lesson 38 Static Members

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,15 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error('Invalid amount');
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value < 0)
-            throw new Error('Invalid value');
-        this._balance = value;
+class Ride {
+    start() { Ride._activeRides++; }
+    stop() { Ride._activeRides--; }
+    static get activeRides() {
+        return Ride._activeRides;
     }
 }
-let account = new Account(1, 'Mo', 0);
-console.log(account.balance);
-account.balance = 2;
+Ride._activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
-class SeatAssignment {
-    // A1, A2 ...
-    // Mustafa, John ...
-    // Index signature property
-    [seatNumber: string]: string
+class Ride {
+    // passenger
+    // pickupLocation
+    // dropOffLocation
+
+    private static _activeRides: number = 0;
+
+    start() {Ride._activeRides++;}
+    stop() {Ride._activeRides--;}
+
+    static get activeRides() {
+        return Ride._activeRides;
+    }
 }
 
-let seats = new SeatAssignment();
-seats.A1 = 'Mustafa';
+
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
