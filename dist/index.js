fb1b70fbd11a1c5dffd302d306b9e855d98b11ff
Mustafa <mustafaanvarov1@gmail.com>
2022-07-13 10:20:53 +0500

    Lesson 37 Index Signatures

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,25 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this._balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid amount');
         this._balance += amount;
     }
-    getBalance() {
+    get balance() {
         return this._balance;
+    }
+    set balance(value) {
+        if (value < 0)
+            throw new Error('Invalid value');
+        this._balance = value;
     }
 }
 let account = new Account(1, 'Mo', 0);
-console.log(account.getBalance());
+console.log(account.balance);
+account.balance = 2;
 //# sourceMappingURL=index.js.map
