a5d4f9843dda3824e3cce8a394914e6fd0c9a45c
Mustafa <mustafaanvarov1@gmail.com>
2022-07-13 16:49:19 +0500

    Lesson 47 Generic Classes

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
+class KeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+class StringKeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+let pair = new StringKeyValuePair('1', 'Apple');
 //# sourceMappingURL=index.js.map
