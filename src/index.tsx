8436a8b5060407f58f1366f92d3a71bd48b6a5c1
Mustafa <mustafaanvarov1@gmail.com>
2022-07-13 16:35:00 +0500

    Lesson 46 Generics

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-// abstract class Calendar {
-//     constructor(public name: string) {}
-
-//     abstract addEvent(): void;
-//     abstract removeEvent(): void
-// }
-
-interface Calendar {
-    name: string;
-    addEvent(): void;
-    removeEvent(): void;
+class KeyValuePair {
+    constructor(public key: number, public value: string) {}
 }
+
+class StringKeyValuePair {
+    constructor(public key: string, public value: string) {}
+}
+
+let pair = new StringKeyValuePair('1', 'Apple');
